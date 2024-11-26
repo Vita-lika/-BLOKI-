63749316572ca38ae49eeb88f90bc8f43b52962e
Vita-lika <vitalinat548@gmail.com>
2024-11-26 10:18:21 +0300

    тт

---
+++ t14.py
@@ -0,0 +1,28 @@
+x = int(input("Введите число: "))
+primer = 2 * x
+print(f"2*{x} = {primer}")
+
+
+a = int(input("Введите длину гипотенузу: "))
+b = int(input("Введите длину противолежащего катета: "))
+otvet = round(b/a)
+print(f"Синус равен: {otvet}")
+
+
+a = int(input("Введите число: "))
+otvet = a*a
+print(f"Ответ: {otvet}")
+
+
+import math
+a = int(input("Введите число: "))
+sqrt = round(math.sqrt(a))
+print(f"Ответ: {sqrt}")
+
+
+import math  
+a = int(input("Введите число: "))
+otvet = round(math.fabs(a))
+print(f"Ваш ответ: {otvet}")
+
+
