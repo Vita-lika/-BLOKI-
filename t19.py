f2bd683e0d811b836d8b74a4b68bff3cee048fe3
Vita-lika <vitalinat548@gmail.com>
2024-12-03 12:52:49 +0300

    bb

---
+++ t19.py
@@ -0,0 +1,39 @@
+# a)
+x = 10
+print(x)
+
+x = -10
+print(x)
+
+# б) 
+x = 17.5
+print(x)
+
+try:
+    x = -2 * x
+except NameError as e:
+    print(e)
+
+# в) 
+x = 60
+print(x)
+
+try:
+    x = x - 1
+except  NameError as e:
+    print(e)
+
+x = 0
+print(x)
+
+# г)
+x = -50
+print(x)
+
+x = -25
+print(x)
+
+try:
+    x = x + k 
+except NameError as e:
+    print(e)
